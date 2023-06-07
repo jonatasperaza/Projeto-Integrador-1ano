c4fb3eba6c990f8198447dfcfe797199aab7c23e
AnthonyLoche <anthonylocheifc@gmail.com>
2023-06-07 15:06:18 -0300

    a

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function click_starwars() {
+    const elemento = document.getElementById("magica");
+    elemento.style.animation = "animation 4s"
+    console.log(click_starwars)
+}
+
+
+
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
